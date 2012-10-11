c97644a5eb35188b8a22b7ecbb49dc525ff0a227
anders <anders@129.16.180.62>
2012-10-11 13:26:46 +0200

    Design almost finished with some connection to beans

--- src/main/webapp/resources/js/jquery_functions.js
+++ src/main/webapp/resources/js/jquery_functions.js
@@ -1,7 +1,16 @@
+/* 
+    Document   : jquery_functions.js
+    Created on : Oct 11, 2012, 01:11:41 PM
+    Author     : Anders
+    Description: Functions using Jquery
+*/
+
+// This is a bug-fix.
 var $ = jQuery;
     
 $(document).ready(function() {
     
+    // Handles the effects of the main menu.
     $("#toggle_arrow").click(function () {
         var elem = "nav#main_menu";
         $(elem).animate({
@@ -16,10 +25,6 @@ $(document).ready(function() {
         {        
             $(this).removeClass('arrow_left');                
             $(this).addClass("arrow_right");                
-        }           
-        
-                 
-                 
-    });   
- 
-});
+        }              
+    });  // toggle end 
+}); // document ready end
