5c2a236546ffd2b38242ed96497962db121663a6
juliar <juliar@ed-3358-05.studat.chalmers.se>
2012-10-22 13:51:57 +0200

    Change Info (still work in progress!) Funkar, men måste lägga till koll av nuvarande password och controll av nytt password (om det är tillräckligt långt osv...)

--- src/main/java/dat076/frukostklubben/bb/ChangeInfoBB.java
+++ src/main/java/dat076/frukostklubben/bb/ChangeInfoBB.java
@@ -7,15 +7,14 @@ package dat076.frukostklubben.bb;
 import dat076.frukostklubben.ejb.UserEJB;
 import dat076.frukostklubben.model.User;
 import java.io.Serializable;
-import java.util.logging.Level;
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.EditableValueHolder;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -24,7 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 @ManagedBean
 @Named("changeInfo")
 @SessionScoped
-public class ChangeInfoBB implements Serializable{
+public class ChangeInfoBB implements Serializable {
 
     // ======================================
     // =             Attributes             =
@@ -34,24 +33,56 @@ public class ChangeInfoBB implements Serializable{
     private User user;
     private String currentMail;
 
+    // ======================================
+    // =           Public Methods           =
+    // ======================================
     public String fetchUser() {
-        String result = "/users/changeInfo?faces-redirect=true";
-        
         user = userRegistry.findByMail(currentMail);
-        
-        return result;
+        return "/users/changeInfo?faces-redirect=true";
     }
 
+    public void change() {
+        userRegistry.updateUser(user);
+    }
+    
+    
+    private String input1;
+    private String input2;
+    private boolean input1Set;
+    
+    public void pwValidator(FacesContext context, UIComponent component,
+            Object value) {
+        if (input1Set) {
+            input2 = (String) value;
+            if (input1.length() < 6) {
+                ((EditableValueHolder) component).setValid(false);
+                context.addMessage(component.getClientId(context), new FacesMessage(
+                        " - Password length > 6 chars"));
+            } else if ((!input1.equals(input2)) || input1 == null) {
+                ((EditableValueHolder) component).setValid(false);
+                context.addMessage(component.getClientId(context), new FacesMessage(
+                        " - Passwords do not match"));
+            }
+        } else {
+            input1Set = true;
+            input1 = (String) value;
+        }
+    }
+
+    // ======================================
+    // =          Getters & Setters         =
+    // ====================================== 
     public User getUser() {
         return user;
     }
 
-    public String getCurrentMail() {
-        return currentMail;
-    }
-
     public void setUser(User user) {
         this.user = user;
+    }
+
+    //needed in order for setPropertyActionListener to work
+    public String getCurrentMail() {
+        return currentMail;
     }
 
     public void setCurrentMail(String currentMail) {
